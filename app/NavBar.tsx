4fa5ab043404c67a9b984ae906fd6bfa8637b4ca
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-08 02:12:27 +0100

    The first commit after setting up navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,33 @@
+
+import { link } from 'fs'
+import Link from 'next/link'
+import React from 'react'
+import {AiFillBug} from "react-icons/ai"
+
+const NavBar = () => {
+    const links = [
+        {label:'Dashboard', href: "/"},
+        {label:'Issues', href: "/issues"},
+    ]
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className="text-zinc-100 hover:text-zinc-400 transition-colors"
+              href={link.href}
+            >
+             {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar
