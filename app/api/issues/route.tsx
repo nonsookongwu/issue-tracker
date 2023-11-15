8bfb6607df3a528746d30d0cc25d447d1987df64
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-15 19:29:18 +0100

    client side form validation

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,52 +1,45 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-import { z } from "zod";
+import { createIssueSchema } from "../../ValidationSchema";
 
-const createIssueSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, "Description is required"),
-});
-
-interface Issue{
-    title: string;
-    description: string;
+interface Issue {
+  title: string;
+  description: string;
 }
 
-
 export async function GET(request: NextRequest) {
-    try {
-      const allIssues = await prisma.issue.findMany();
+  try {
+    const allIssues = await prisma.issue.findMany();
 
-      return NextResponse.json(allIssues);
-    } catch (error) {
-      return NextResponse.json(error, { status: 500 });
-    }
+    return NextResponse.json(allIssues);
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
 }
 
 export async function POST(request: NextRequest) {
-    try {
-        const body: Issue = await request.json();
+  try {
+    const body: Issue = await request.json();
 
-        const validation = createIssueSchema.safeParse(body);
+    const validation = createIssueSchema.safeParse(body);
 
-        if (!validation.success) {
-          return NextResponse.json(
-            { error: validation.error.errors[0].message },
-            { status: 400 }
-          );
-        }
+    if (!validation.success) {
+      return NextResponse.json(
+        { error: validation.error.errors[0].message },
+        { status: 400 }
+      );
+    }
 
-        const issue = await prisma.issue.create({
-          data: {
-            title: body.title,
-            description: body.description,
-          },
-        });
+    const issue = await prisma.issue.create({
+      data: {
+        title: body.title,
+        description: body.description,
+      },
+    });
 
-        return NextResponse.json(issue, { status: 201 });
-    
-   } catch (error) {
-    return NextResponse.json(error, {status: 500})
-   }
+    return NextResponse.json(issue, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,23 +7,29 @@ import "easymde/dist/easymde.min.css";
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/ValidationSchema';
+import { z } from 'zod'
 
-interface IssuesForm{
-  title: string;
-  description: string;
-}
+// interface IssuesForm{
+//   title: string;
+//   description: string;
+// }
+type IssuesForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
 
   const router = useRouter()
 
-  const { register, control, handleSubmit } = useForm<IssuesForm>()
+  const { register, control, handleSubmit, formState:{errors} } = useForm<IssuesForm>({
+   resolver: zodResolver(createIssueSchema)
+  })
   const [error, setError] = useState("")
 
   return (
     <div className="max-w-xl">
       {error && (
-        <Callout.Root size={"1"} color="red" className='mb-5'>
+        <Callout.Root size={"1"} color="red" className="mb-5">
           <Callout.Icon>
             <InfoCircledIcon />
           </Callout.Icon>
@@ -44,14 +50,14 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Create issue" {...register("title")} />
         </TextField.Root>
-        {/* {error && (
-          <Callout.Root size={"1"}>
+        {errors.title && (
+          <Callout.Root size={"1"} color="red" className="mb-7">
             <Callout.Icon>
               <InfoCircledIcon />
             </Callout.Icon>
-            <Callout.Text>{error}</Callout.Text>
+            <Callout.Text>{errors.title.message}</Callout.Text>
           </Callout.Root>
-        )} */}
+        )}
 
         <Controller
           name="description"
@@ -60,6 +66,14 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Callout.Root size={"1"} color="red" className="mb-7">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>{errors.description.message}</Callout.Text>
+          </Callout.Root>
+        )}
 
         <Button>Submit New Issue</Button>
       </form>
