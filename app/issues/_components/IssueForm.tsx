ff318fcc8434ce210e982875a1880d6a7b05fa91
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-23 02:16:47 +0100

    remove duplicate skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -42,7 +42,7 @@ const IssueForm = ({ issue }: { issue?: Issue | null }) => {
         router.push(`/issues/${issue.id}`)
       } else {
         await axios.post("/api/issues", data);
-        router.push("/issues");
+        router.push("/issues/list");
       }
 
         router.refresh();
