f83c7979bd81451b9299a98a5e70c00fe6b0fba4
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-23 02:01:49 +0100

    improved the UX by adding a delete loading

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,7 @@
 "use client";
+import Spinner from "@/app/components/Spinner";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -11,16 +11,18 @@ interface Props {
 
 const DeleteIssueButton = ({ issueId }: Props) => {
     const router = useRouter();
-    const [error, setError] = useState(false)
+  const [error, setError] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false)
 
   const handleDeleteissue = async () => {
       try {
-          throw new Error
+        setIsDeleting(true)
         await axios.delete(`/api/issues/${issueId}`);
         router.push("/issues");
         router.refresh();
           
       } catch (error) {
+        setIsDeleting(false)
           setError(true)
     }
   };
@@ -30,7 +32,8 @@ const DeleteIssueButton = ({ issueId }: Props) => {
       <>
         <AlertDialog.Root>
           <AlertDialog.Trigger>
-            <Button color="red">Delete Issue</Button>
+            <Button color="red" disabled={isDeleting}>
+              {isDeleting ? "Deleting..." : 'Delete Issue'} { isDeleting && <Spinner/>}</Button>
           </AlertDialog.Trigger>
           <AlertDialog.Content>
             <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
