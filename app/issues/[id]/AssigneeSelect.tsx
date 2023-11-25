712e441ff9ca9d9951f792eefbbc976ce5b9ea81
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-25 18:31:59 +0100

    fetch data with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,36 +1,44 @@
-"use client"
-import { User } from '@prisma/client'
-import { Select } from '@radix-ui/themes'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 
 const AssigneeSelect = async () => {
-    const [users, setUsers] = useState<User[]>([])
-
-    useEffect(() => {
-      
-        const fetchUsers = async () => {
-           const {data} = await axios.get<User[]>("/api/users");
-           setUsers(data); 
-        }
-
-        fetchUsers();
-    }, [])
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60seconds
+    retry: 3,
+  });
     
-    
+  
+  if (error) return null
+  
+  if(isLoading) return <Skeleton/>
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign ..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
-              <Select.Item key={user.id} value={ user.id}>{ user.name}</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
           ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
   );
-}
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
