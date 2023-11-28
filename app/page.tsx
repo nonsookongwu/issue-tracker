d07cd534c43add30e9ae15021b8d2ff7c064b285
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-28 20:05:03 +0100

    built the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Pagination from './components/Pagination';
 
 export default function Home() {
   return (
     <div>
       <h1> Hello world</h1>
+
+      <Pagination currentPage={2} itemCount={100} pageSize={10}/>
     </div>
   );
 }
