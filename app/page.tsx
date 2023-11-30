074e6158afb2800fbff2d31d804f1fff091ea312
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-30 04:22:54 +0100

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
 import Pagination from './components/Pagination';
 import LatestIssues from './LatestIssues';
+import IssueSummary from './IssueSummary';
+import prisma from '@/prisma/client';
 
 
-export default function Home() {
+export default async function Home() {
+
+ const openIssues = await prisma.issue.count({where:{status:'OPEN'}})
+ const inProgressIssues = await prisma.issue.count({where:{status:'IN_PROGRESS'}})
+ const closedIssues = await prisma.issue.count({where:{status:'CLOSED'}})
+
   return (
     <div>
-    <LatestIssues/>
+      {/* <LatestIssues/> */}
+      <IssueSummary closed={closedIssues} inProgress={inProgressIssues} open={openIssues}/>
       
     </div>
   );
