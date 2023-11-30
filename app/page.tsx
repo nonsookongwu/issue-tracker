48fc095c435ef8adeb3fc6a984188a4d10f561c8
nonsookongwu <nonso.okongwu@gmail.com>
2023-11-30 04:58:03 +0100

    build the issue charts feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Pagination from './components/Pagination';
 import LatestIssues from './LatestIssues';
 import IssueSummary from './IssueSummary';
 import prisma from '@/prisma/client';
+import IssueChart from './IssueChart';
 
 
 export default async function Home() {
@@ -14,8 +15,16 @@ export default async function Home() {
   return (
     <div>
       {/* <LatestIssues/> */}
-      <IssueSummary closed={closedIssues} inProgress={inProgressIssues} open={openIssues}/>
-      
+      <IssueSummary
+        closed={closedIssues}
+        inProgress={inProgressIssues}
+        open={openIssues}
+      />
+      <IssueChart
+        closed={closedIssues}
+        inProgress={inProgressIssues}
+        open={openIssues}
+      />
     </div>
   );
 }
